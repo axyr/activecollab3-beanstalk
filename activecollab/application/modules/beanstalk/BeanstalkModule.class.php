535a7646c149fc11356d0878ba15b3efc55229bd
Andreas Schempp <andreas@schempp.ch>
2012-01-10 11:29:28 +0100

    Totally rewritten version 2. Now supports Beanstalk commit hooks, but no longer can deploy from activeCollab.

--- activecollab/application/modules/beanstalk/BeanstalkModule.class.php
+++ activecollab/application/modules/beanstalk/BeanstalkModule.class.php
@@ -1,134 +1,140 @@
 <?php
 
+/**
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  Andreas Schempp 2012
+ * @author 	Andreas Schempp <andreas@schempp.ch>
+ * @license	http://opensource.org/licenses/lgpl-3.0.html
+ */
+
 
 class BeanstalkModule extends Module
 {
-    
-    /**
-     * Plain module name
-     *
-     * @var string
-     */
-    var $name = 'beanstalk';
-    
-    /**
-     * Is system module flag
-     *
-     * @var boolean
-     */
-    var $is_system = false;
-    
-    /**
-     * Module version
-     *
-     * @var string
-     */
-    var $version = '1.0';
-    
-    // ---------------------------------------------------
-    //  Events and Routes
-    // ---------------------------------------------------
-    
-    
-    /**
-     * Define module routes
-     *
-     * @param Router $r
-     * @return null
-     */
-    function defineRoutes(&$router)
-    {
-    	$router->map('project_repositories', '/projects/:project_id/repositories', array('controller'=>'beanstalk', 'action'=>'index'), array('project_id'=>'\d+'));
-    	$router->map('beanstalk_deploy_widget', '/projects/:project_id/repositories/:repository_id/beanstalk-deploy', array('controller'=>'beanstalk', 'action'=>'deploy_widget'), array('project_id'=>'\d+', 'repository_id'=>'\d+'));
-    } 
-    
-    
-    /**
-     * Can this module be installed or not
-     *
-     * @param array $log
-     * @return boolean
-     */
+	
+	/**
+ 	* Plain module name
+ 	*
+ 	* @var string
+ 	*/
+	var $name = 'beanstalk';
+	
+	/**
+ 	* Is system module flag
+ 	*
+ 	* @var boolean
+ 	*/
+	var $is_system = false;
+	
+	/**
+ 	* Module version
+ 	*
+ 	* @var string
+ 	*/
+	var $version = '2.0';
+	
+	// ---------------------------------------------------
+	//  Events and Routes
+	// ---------------------------------------------------
+	
+	
+	/**
+ 	* Define module routes
+ 	*
+ 	* @param Router $r
+ 	* @return null
+ 	*/
+	function defineRoutes(&$router)
+	{
+		$router->map('beanstalk_commit', '/projects/:project_id/beanstalk/commit', array('controller'=>'beanstalk', 'action'=>'commit'), array('project_id'=>'\d+'));
+	} 
+	
+	
+	/**
+ 	* Can this module be installed or not
+ 	*
+ 	* @param array $log
+ 	* @return boolean
+ 	*/
 	function canBeInstalled(&$log)
 	{
-		if(extension_loaded('SimpleXML') && function_exists('simplexml_load_string'))
+		if (function_exists('json_decode'))
 		{
-			$log[] = lang('OK: SimpleXML extension loaded');
-			
-			if(extension_loaded('curl') && function_exists('curl_init'))
-			{
-				$log[] = lang('OK: CURL extension loaded');
-			}
-			else
-			{
-				$log[] = lang('This module requires CURL PHP extension to be installed. Read more about CURL extension in PHP documentation: http://www.php.net/manual/en/book.curl.php');
-			
-				return false;
-			}
+			$log[] = lang('OK: json_decode() function available');
 			
 			return true;
 		}
 		else
 		{
-			$log[] = lang('This module requires SimpleXML PHP extension to be installed. Read more about SimpleXML extension in PHP documentation: http://www.php.net/manual/en/book.simplexml.php');
+			$log[] = lang('This module requires PHP json_decode() function to be installed. Read more about json_decode() in PHP documentation: http://www.php.net/json_decode');
 			
 			return false;
 		}
 	}
-    
-    
-    /**
-     * Install this module
-     *
-     * @param void
-     * @return boolean
-     */
-    function install()
-    {
-     	if (parent::install())
-     	{
-     		$objModule = Modules::findById('beanstalk');
-     		$objModule->setPosition(1000);
-     		$objModule->save();
-     		return true;
-     	}
-     	
-     	return false;
-    }
-    
-    
-    /**
-     * Get module display name
-     *
-     * @return string
-     */
-    function getDisplayName()
-    {
+	
+	
+	/**
+	* Install this module
+	*
+	* @param void
+	* @return boolean
+	*/
+	function install()
+	{
+		if (parent::install())
+		{
+			return true;
+		}
+		
+		return false;
+	}
+	
+	
+	/**
+	* Get module display name
+	*
+	* @return string
+	*/
+	function getDisplayName()
+	{
 		return lang('Beanstalk');
-    }
-    
-    
-    /**
-     * Return module description
-     *
-     * @param void
-     * @return string
-     */
-    function getDescription()
-    {
-		return lang('Extend source module with beanstalk features.');
-    }
-    
-    
-    /**
-     * Return module uninstallation message
-     *
-     * @param void
-     * @return string
-     */
-    function getUninstallMessage()
-    {
-    	return lang('Module will be deactivated. You will no longer be able to deploy.');
-    }
+	}
+	
+	
+	/**
+	* Return module description
+	*
+	* @param void
+	* @return string
+	*/
+	function getDescription()
+	{
+		return lang('Adds Beanstalk post-deployment hook support. Can post ticket comments and track time based on your commit message.');
+	}
+	
+	
+	/**
+	* Return module uninstallation message
+	*
+	* @param void
+	* @return string
+	*/
+	function getUninstallMessage()
+	{
+		return lang('Module will be deactivated. You will no longer be able to deploy.');
+	}
 }
 
--- activecollab/application/modules/beanstalk/controllers/BeanstalkController.class.php
+++ activecollab/application/modules/beanstalk/controllers/BeanstalkController.class.php
@@ -1,9 +1,8 @@
 <?php
 
-use_controller('repository', SOURCE_MODULE);
+use_controller('project', SYSTEM_MODULE);
 
-
-class BeanstalkController extends RepositoryController
+class BeanstalkController extends ProjectController
 {
 
 	/**
@@ -20,126 +19,218 @@ class BeanstalkController extends RepositoryController
 	*/
 	var $active_module = BEANSTALK_MODULE;
 	
+	/**
+ 	* Array of controller actions that can be accessed through API
+ 	*
+ 	* @var array
+ 	*/
+	var $api_actions = array('commit');
+	
  
-	function deploy_widget()
+	function commit()
 	{
-		$this->skip_layout = true;
+		$varValue = file_get_contents("php://input");
 		
-		preg_match('@(http[s]?)://([a-z]+)\.svn\.beanstalkapp.com/(.+)@', $this->active_repository->getUrl(), $arrMatches);
-		
-		$mode = $arrMatches[1];
-		$domain = $arrMatches[2];
-		$repository_url = explode('/', $arrMatches[3]);
-		$repository_name = $repository_url[0];
-		$repository_id = 0;
-
-
-		if ($this->request->isSubmitted())
+		if ($varValue == '')
 		{
-			$deploy = $this->request->post('deploy');
-			
-			$postData = 
-'<release>
-  <revision type="integer">' . $deploy['revision'] . '</revision>
-  <comment>' . $deploy['comment'] . '</comment>' . ($deploy['deploy_from_scratch'] ? '
-  <deploy_from_scratch>1</deploy_from_scratch>' : '') . '
-</release>';
-
-			if ($this->http_request_xml($mode, $domain, ($repository_name.'/releases.xml?environment_id='.$deploy['environment_id']), $postData))
-			{
-				flash_success("Deployment of repository " . $this->active_repository->getName() . " has been initiated.");
-			}
-			else
-			{
-				flash_error("Deployment of repository " . $this->active_repository->getName() . " failed.");
-			}
-			
-			$this->redirectTo('project_repositories', array('project_id'=>$this->active_project->getId()));
+			exit;
 		}
 		
-		$xml = $this->http_request_xml($mode, $domain, 'repositories.xml');
+		$objData = json_decode($varValue);
 		
-		foreach( $xml->repository as $repository )
+		if (!is_object($objData))
 		{
-			if ($repository->name == $repository_name)
+			header('HTTP/1.1 400 Bad Request');
+			die('HTTP/1.1 400 Bad Request');
+		}
+		
+		// GIT commit
+		if (is_array($objData->commits))
+		{
+			foreach( $objData->commits as $objCommit )
 			{
-				$repository_id = $repository->id;
-				break;
+				$strMessage = $this->generate_comment((string) $objCommit->message, (string) $objCommit->url, (string) $objCommit->id);
+				$this->handle_commit((string) $objCommit->timestamp, $strMessage, (string) $objCommit->author->email, (string) $objCommit->author->name);
 			}
 		}
 		
-		if ($repository_id == 0)
+		// SVN commit
+		else
 		{
-			// Repository not found
+			$strMessage = $this->generate_comment((string) $objData->message, (string) $objData->changeset_url, (string) $objData->revision);
+			$this->handle_commit((string) $objData->time, $strMessage, (string) $objData->author_email, (string) $objData->author_full_name);
 		}
 		
-		$servers = array();
-		
-		$xml = $this->http_request_xml($mode, $domain, $repository_id.'/server_environments.xml');
-		
-		foreach( $xml->{'server-environment'} as $server )
-		{
-			$server_name = strval($server->name);
-			
-			if ($server->{'current-version'})
-			{
-				$server_name .= ' - Revision ' . $server->{'current-version'} . ' deployed on ' . date('d.m.Y H:i', strtotime($server->{'updated-at'}));
-			}
-			
-			$servers[intval($server->id)] = $server_name;
-		}
-		
-		$commit = Commits::findLastCommit($this->active_repository);
-		
-		$latest_revision = $commit->values[$commit->field_map['revision']];
-		
-		$this->smarty->assign(array(
-			'action' => '/'.ANGIE_PATH_INFO,
-			'servers' => $servers,
-			'revisions' => range($latest_revision, ($latest_revision-10)),
-		));
+		exit;
 	}
-  
-  
-  
-    function http_request_xml($mode, $domain, $file, $data=null)
-    {
-    	$ch = curl_init($mode . '://' . $domain . '.beanstalkapp.com/api/' . $file);
+	
+	
+	function handle_commit($strDate, $strMessage, $strAuthorEmail='', $strAuthorName='')
+	{
+		$objUser = Users::findByEmail($strAuthorEmail);
 		
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-		curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-		curl_setopt($ch, CURLOPT_USERPWD, $this->active_repository->getUsername() . ':' . $this->active_repository->getPassword());
-		
-		if ($data)
+		// Commit message [#15 tagged:committed responsible:chris milestone:"Beta Release" state:resolved time:2.00]
+		if (preg_match('/\[(#([\d]+))?([^\]]*)\]/is', $strMessage, $arrMatches))
 		{
-			curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: text/xml'));
-			curl_setopt($ch, CURLOPT_POST, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-			curl_exec($ch);
+			$strMessage = trim(str_replace($arrMatches[0], '', $strMessage));
+			$fltTime = $this->parse_time($arrMatches[3]);
 			
-			$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+			$objTicket = null;
+			$intTicket = (int) $arrMatches[2];
+
+			if ($intTicket > 0)
+			{
+				$objTicket = Tickets::findByTicketId($this->active_project, $intTicket);
+			}
 			
-			if ($code != 201)
-				return false;
+			if (instance_of($objTicket, 'Ticket'))
+			{
+				$this->addComment($objTicket, $strDate, $strMessage, $objUser, $strAuthorName, $strAuthorEmail);
+				
+				if ($fltTime !== false && instance_of($objUser, 'User'))
+				{
+					$this->addTime($objUser, $fltTime, $strDate, $strMessage, $objTicket);
+				}
+				
+				return;
+			}
 			
-			return true;
+			if ($fltTime !== false && instance_of($objUser, 'User'))
+			{
+				$this->addTime($objUser, $fltTime, $strDate, $strMessage);
+			}
+			
+			return;
 		}
 		
-		$data = curl_exec($ch);
-		$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 		
-		curl_close($ch);
-
-		switch( $code )
+		// Commit Message (Ticket #15)
+		if (preg_match('/\(?(complete[d]?[\s]+)?ticket[\s]+[#]?(\d+):?\)?/is', $strMessage, $arrMatches))
 		{
-			case '200':
-				$obj = simplexml_load_string($data);
-				return $obj;
-				break;
+			$strMessage = trim(str_replace($arrMatches[0], '', $strMessage));
+			
+			$objTicket = null;
+			$intTicket = (int) $arrMatches[2];
+
+			if ($intTicket > 0)
+			{
+				$objTicket = Tickets::findByTicketId($this->active_project, $intTicket);
+			}
+			
+			if (instance_of($objTicket, 'Ticket'))
+			{
+				$this->addComment($objTicket, $strDate, $strMessage, $objUser, $strAuthorName, $strAuthorEmail);
+			}
+			
+			return;
+		}
+	}
+	
+	
+	function parse_time($strMessage)
+	{
+		// Find time in message like "#15 time:2.00"
+		$arrTokens = array_map('trim', explode(' ', strtolower($strMessage)));
+		if (!empty($arrTokens))
+		{
+			foreach( $arrTokens as $strToken )
+			{
+				list($strKey, $strValue) = array_map('trim', explode(':', $strToken));
+				
+				if ($strKey == 'time')
+				{
+					return round($strValue, 2);
+				}
+			}
+		}
+		
+		// Find time amount in brackets
+		if (preg_match('/\(([\d]+[\.|,][\d]+?)\)/is', $strMessage, $arrMatches))
+		{
+			return round(str_replace(',', '.', $arrMatches[1]), 2);
 		}
 		
 		return false;
-    }
+	}
+	
+	
+	function generate_comment($strMessage, $strUrl, $strId)
+	{
+		// Shorten GIT commit id
+		$strId = substr($strId, 0, 8);
+		
+		return '
+<p><strong>'.$strId.':</strong> '.$strMessage.'</p>
+<p><a href="'.$strUrl.'" target="_blank" title="See changeset '.$strId.'">See changeset on Beanstalk</a></p>';
+	}
+	
+	
+	function addComment($objParent, $strDate, $strMessage, $objUser=null, $strUserName='', $strUserEmail='')
+	{
+		$objComment = new Comment();
+		$objComment->log_activities = false;
+		$objComment->setParent($objParent);
+		
+		$objComment->setBody($strMessage);
+		$objComment->setCreatedOn($strDate);
+		$objComment->setProjectId($this->active_project->getId());
+		$objComment->setState(STATE_VISIBLE);
+		$objComment->setVisibility($objParent->getVisibility());
+		
+		if (instance_of($objUser, 'User'))
+		{
+			$objComment->setCreatedBy($objUser);
+		}
+		else
+		{
+			$objComment->setCreatedById(0);
+			$objComment->setCreatedByName($strUserName);
+			$objComment->setCreatedByEmail($strUserEmail);
+		}
+		
+		$save = $objComment->save();
+
+		if ($save && !is_error($save))
+		{
+			$objComment->ready();
+		}
+		
+		return $save;
+	}
+	
+	
+	function addTime($objUser, $fltTime, $strDate, $strMessage, $objParent=null)
+	{
+		if (!instance_of($objUser, 'User'))
+		{
+			return false;
+		}
+					
+		$objTimeRecord = new TimeRecord();
+		
+		if ($objParent !== null)
+		{
+   			$objTimeRecord->setParent($objParent);
+		}
+		
+		$timetracking_data = array
+		(
+			'user_id'			=> $objUser->getId(),
+			'record_user'		=> $objUser,
+			'record_date'		=> $strDate,
+			'value'				=> $fltTime,
+			'billable_status'	=> BILLABLE_STATUS_BILLABLE,
+			'body'				=> $strMessage,
+		);
+		
+		$objTimeRecord->setAttributes($timetracking_data);
+		$objTimeRecord->setProjectId($this->active_project->getId());
+		$objTimeRecord->setCreatedBy($objUser);
+		$objTimeRecord->setState(STATE_VISIBLE);
+		$objTimeRecord->setVisibility(VISIBILITY_NORMAL);
+		$objTimeRecord->setUser($objUser);
+		
+		return $objTimeRecord->save();
+	}
 }
 
